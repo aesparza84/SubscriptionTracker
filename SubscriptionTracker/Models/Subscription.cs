9d347570368252cf437af9b65b509cdae92dc31f
axe53 <agitty84.ae@gmail.com>
2025-06-03 17:36:22 -0500

    init

---
+++ SubscriptionTracker/Models/Subscription.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public enum Category {Streaming, Personal, Gaming}
+    public enum RenewalType {Monthly, Weekly, Annual}
+    public class Subscription
+    {
+        public required string Name { get; set; }
+        public required double Price { get; set; }   
+        public required Category Category { get; set; }
+        public required RenewalType RenewalType { get; set; }
+        public required DateTime RenewalDate { get; set; }
+    }
+}
---
+++ SubscriptionTracker/SubscriptionTracker/View/UserControls/ClearableTextBox.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SubscriptionTracker.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for ClearableTextBox.xaml
+    /// </summary>
+    public partial class ClearableTextBox : UserControl
+    {
+        /// <summary>
+        /// Place holder property for the text box
+        /// </summary>
+        private string placeHolder;
+        public string PlaceHolder
+        {
+            get { return placeHolder; }
+            set {
+                placeHolder = value;
+                tbPlaceHolder.Text = placeHolder; 
+            }
+        }
+
+        public ClearableTextBox()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtInput.Clear();
+            txtInput.Focus();
+        }
+
+        private void txtInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtInput.Text))
+            {
+                tbPlaceHolder.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                tbPlaceHolder.Visibility = Visibility.Hidden;
+            }
+        }
+    }
+}
