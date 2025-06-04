94e8c75890786747c75b325e6491f02f5566532b
axe53 <agitty84.ae@gmail.com>
2025-06-04 15:34:20 -0500

    clean up

--- SubscriptionTracker/Models/Subscription.cs
+++ SubscriptionTracker/Models/Subscription.cs
@@ -6,14 +6,12 @@ using System.Threading.Tasks;
 
 namespace Models
 {
-    public enum Category {Streaming, Personal, Gaming}
-    public enum RenewalType {Monthly, Weekly, Annual}
     public class Subscription
     {
         public required string Name { get; set; }
         public required double Price { get; set; }   
-        public required Category Category { get; set; }
-        public required RenewalType RenewalType { get; set; }
+        public required CategoryTypeEnum Category { get; set; }
+        public required RenewTypeEnum RenewalType { get; set; }
         public required DateTime RenewalDate { get; set; }
     }
 }
